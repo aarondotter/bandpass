275a5c5a92232a3d4cb232c922174e7d3451af12
Aaron Dotter <aaron.dotter@gmail.com>
2014-08-27 14:11:45 +1000

    first commit; source code and lists for photometric systems

---
+++ src/extinction.f
@@ -0,0 +1,76 @@
+      module extinction
+      
+      ! provides the Rv-dependent extinction curve from Rv = Av/E(B-V)
+
+      ! Cardelli, Clayton, & Mathis (1989, ApJ, 345, 245-256)
+
+      ! Al_div_Av_CCM provides <A(lambda)/A(V)> as a function of input 
+      ! lambda and Rv, with lambda in Angstroms.
+
+      implicit none
+
+      private
+
+      public :: Al_div_Av_CCM
+
+      contains
+
+      !elemental: can be called on a scalar or an array in the same way
+      elemental function Al_div_Av_CCM(lambda,Rv) result(Alam)
+         double precision, intent(in) :: lambda !in angstrom
+         double precision, intent(in) :: Rv ! Av/E(B-V)
+         double precision :: a, b, x, y, Alam
+         double precision, parameter :: small = 1d-8, min_x = 0d0 ! 2d-1
+        
+         ! x is wavenumber in 1/micron : x = 10^4/lambda
+         ! impose an upper bound in the UV
+         x = min(1d1,1d4/lambda)
+
+         ! CCM break up the extinction curve into 4 regions in x
+         if (x>=min_x .and. x<= 1.1d0) then ! Infrared
+         ! strictly speaking, CCM imposes a limit on x >= 0.2 but we ignore this here
+         ! in order to allow for mid-IR wavelengths.
+            a = 0.574d0*x**1.61d0
+            b = -0.527d0*x**1.61d0
+         else if ( x> 1.1d0 .and. x<=3.3d0) then !Optical
+            y = x - 1.82d0            
+            a = 1d0 + 1.7699d-1*y - 5.0447d-1*y**2 - 2.427d-2*y**3 &
+            + 7.2085d-1*y**4 + 1.979d-2*y**5 - 7.7530d-1*y**6 + 3.2999d-1*y**7
+            b = 1.41338d0*y + 2.28305d0*y**2 + 1.07233d0*y**3 &
+            - 5.38434d0*y**4 - 6.2251d-1*y**5 + 5.30260d0*y**6 - 2.09002d0*y**7 
+         else if (x> 3.3d0 .and. x<8d0) then !Ultraviolet
+            a = 1.752d0 - 3.16d-1*x - 1.04d-1/((x-4.67d0)**2 + 3.41d-1) + Fa(x)
+            b = -3.090d0 + 1.825d0*x + 1.206d0/( (x-4.62d0)**2 + 2.63d-1) + Fb(x)
+         else if (x> 8d0 .and. x <= 1d1) then !Far-Ultraviolet
+            y = x - 8d0
+            a = -1.073d0 - 6.28d-1*y + 1.37d-1 * y**2 - 7d-2*y**3
+            b = 13.67d0 + 4.257d0*y - 4.2d-1*y**2 + 3.74d-1*y**3
+         else !return zero if outside defined range
+            a = 0d0
+            b = 0d0
+         endif
+         Alam = a + b/Rv   
+      end function Al_div_Av_CCM
+         
+      !special functions Fa and Fb for UV region
+      elemental function Fa(x) result(y)
+         double precision, intent(in) :: x
+         double precision :: y
+         if(x<5.9d0)then
+            y = 0d0
+         else
+            y = -4.473d-2*(x-5.9d0)**2 - 9.779d-3*(x-5.9d0)**3
+         endif
+      end function Fa
+
+      elemental function Fb(x) result(y)
+         double precision, intent(in) :: x
+         double precision :: y
+         if(x<5.9d0)then
+            y = 0d0
+         else
+            y = 2.13d-1*(x-5.9)**2 + 1.207d-1*(x-5.9d0)**3
+         endif
+      end function Fb
+
+      end module extinction
---
+++ src/grid_bandpass.f
@@ -0,0 +1,394 @@
+      program test_bandpass
+
+      use bandpass
+
+      implicit none
+      character(len=256) :: filter_list, my_data_dir
+      character(len=16) :: suffix, arg
+      integer :: count, choice, phot_system
+      logical :: do_CNONa = .false., do_NGC6752 = .false., do_BTSettl=.true.
+      !integer, parameter:: num_Av = 4, num_Rv = 1
+      integer, parameter :: num_Av=13, num_Rv=3
+      double precision :: Av(num_Av), Rv(num_Rv)
+      
+      !Av = [ 0.0d0, 0.12d0, 0.17d0, 0.15d0 ]
+      !Rv = [  3.1d0 ]
+      Av = [ 0d0, 0.1d0, 0.2d0, 0.4d0, 0.6d0, 0.8d0, &
+             1d0, 2d0, 4d0, 6d0, 8d0, 1d1, 1.2d1 ]
+      Rv = [ 2d0, 3.1d0, 4d0 ]
+
+      count = command_argument_count()
+
+      my_data_dir='/home/dotter/science/Spectra'
+      !my_data_dir='/coala/dotter/science/Spectra'
+
+      call set_data_dir(my_data_dir)
+
+      if(count>1)then
+         !process command line arguments
+         call get_command_argument(1,arg)
+         read(arg,*) choice
+         call get_command_argument(2,arg)
+         read(arg,*) phot_system
+
+         call setup(phot_system)
+
+         if(choice==VEGAZP)then
+            call do_vega(filter_list)
+         else
+            call do_one_set(choice,filter_list,suffix)
+         endif
+      else
+         call write_usage_details
+      endif
+
+      contains
+      
+      subroutine do_vega(filter_list)
+      character(len=256), intent(in) :: filter_list
+      character(len=13), pointer :: filter_name(:)
+      type(spectrum) :: vega, AB, ST
+      type(spectrum), allocatable :: filter_set(:)
+      integer :: i, ierr, i0, i1, num_filters
+      double precision, allocatable :: Vega_ZP(:), ST_ZP(:), AB_ZP(:)
+
+      if(debug) write(*,*) '     read vega . . .'
+      vega% filename=vega_filename
+      AB% filename = vega_filename
+      ST% filename = vega_filename
+      write(0,*)"files: ",trim(vega% filename),trim(AB% filename),trim(ST% filename)
+      call read_vega(vega,AB,ST,ierr)
+      if(ierr/=0) stop 'failed in read_vega'
+      if(debug)then
+         write(*,*) '   filename = ', trim(vega% filename)
+         write(*,*) '   npts = ', vega% npts
+         write(*,*) vega% wave(1), vega% flux(1)
+         write(*,*) vega% wave(vega% npts), vega% flux(vega% npts)
+      endif
+
+      if(debug) write(*,*) '      read filters . . .'
+      call read_filters(filter_list,filter_set,num_filters,ierr)
+      if(ierr/=0) stop 'failed in read_filters'
+
+      allocate(Vega_ZP(num_filters),filter_name(num_Filters),ST_ZP(num_filters),AB_ZP(num_filters))
+
+      if(debug) write(*,*) '     derive zeropoints . . .'
+      write(*,'(a3,3x,a13,99a20)') 'i', 'filter', 'Vega ZP', 'ST ZP', 'AB ZP', 'mag(Vega/ST)', &
+                                   'mag(Vega/AB)'
+      do i=1,num_Filters !'
+         i0 = index(filter_set(i)% filename, '/', .true.)+1
+         i1 = index(filter_set(i)% filename, '.', .true.)-1
+         filter_name(i) = filter_set(i)% filename(i0:i1)
+         Vega_ZP(i) = integrate_bandpass(vega,filter_set(i),ierr)
+         ST_ZP(i) = integrate_bandpass(ST,filter_set(i),ierr)
+         AB_ZP(i) = integrate_bandpass(AB,filter_set(i),ierr)
+         if(ierr/=0) stop 'failed in integrate_bandpass'
+         write(*,'(i3,3x,a13,99f20.10)') i, filter_name(i), &
+                                                   Vega_ZP(i), ST_ZP(i), AB_ZP(i), &
+                                                -2.5d0*log10(Vega_ZP(i)/ST_ZP(i)), &
+                                                -2.5d0*log10(Vega_ZP(i)/AB_ZP(i))
+      enddo
+
+      deallocate(vega_zp,filter_name)
+
+      end subroutine do_vega
+
+      subroutine do_one_set(choice,filter_list,suffix)
+      integer, intent(in) :: choice
+      character(len=256), intent(in) :: filter_list
+      character(len=16), intent(in) :: suffix
+      character(len=256) :: spectra_list, outfile, filename, prefix, line
+      character(len=10), allocatable :: filter_name(:)
+      character(len=2) :: cRv
+      type(spectrum) :: vega, AB, ST
+      type(spectrum), allocatable :: filter(:)
+      type(spectrum), pointer :: spectra(:)
+      integer :: i, ierr, i0, i1, j, k, l, num_filters, num_spectra
+      double precision :: tmp_mag
+      double precision, allocatable :: ZP(:)
+      double precision, pointer :: mag(:,:,:,:)
+      logical :: AOK
+
+      ierr=0
+
+      select case(choice)
+      case(PHOENIX)
+         if(do_CNONa)then
+            write(*,*) '    doing CNONa . . . '
+            spectra_list = trim(data_dir)//'/lists/phoenix_CNONa.list'
+         else if(do_NGC6752)then
+            write(*,*) '    doing NGC6752 . . . '
+            spectra_list = trim(data_dir)//'/lists/NGC6752.list'
+         else if(do_BTSettl)then
+            write(*,*) '    doing BT-Settl . . . '
+            spectra_list = trim(data_dir)//'/lists/BTSettl.list'
+         else
+            write(*,*) '    doing PHOENIX . . . '
+            spectra_list = trim(data_dir)//'/lists/phoenix.list'
+         endif
+      case(CK2003)
+         write(*,*)    '    doing Castelli & Kurucz . . . '
+         spectra_list = trim(data_dir)//'/lists/odfnew.list'
+      case(ATLAS)
+         write(*,*)    '    doing ATLAS/Synthe . . . '
+         spectra_list = trim(data_dir) // '/lists/ATLAS.list'
+      end select
+
+      if(debug) write(*,*) '     read vega . . .'
+      vega% filename=vega_filename
+
+      call read_vega(vega,AB,ST,ierr)
+      if(ierr/=0) stop 'failed in read_vega'
+      if(debug)then
+         write(*,*) '   filename = ', trim(vega% filename)
+         write(*,*) '   npts = ', vega% npts
+         write(*,*) vega% wave(1), vega% flux(1)
+         write(*,*) vega% wave(vega% npts), vega% flux(vega% npts)
+      endif
+
+      if(debug) write(*,*) '      read filters . . .'
+      call read_filters(filter_list,filter,num_filters,ierr)
+      if(ierr/=0) stop 'failed in read_filters'
+
+      allocate(ZP(num_filters),filter_name(num_Filters))
+
+      if(debug) write(*,*) '     integrate bandpasses . . .'
+      do i=1,num_Filters
+         i0 = index(filter(i)% filename, '/', .true.)+1
+         i1 = index(filter(i)% filename, '.', .true.)-1
+         filter_name(i) = filter(i)% filename(i0:i1)
+         select case(zero_point_type)
+            case(zero_point_Vega)
+               ZP(i) = integrate_bandpass(vega,filter(i),ierr)
+            case(zero_point_ST)
+               ZP(i) = integrate_bandpass(ST,filter(i),ierr)
+            case(zero_point_AB)
+               ZP(i) = integrate_bandpass(AB,filter(i),ierr)
+            case default
+               stop 'incorrect choice of zero point!'
+         end select
+         if(ierr/=0) stop 'failed in integrate_bandpass'
+         if(debug) write(*,'(i3,3x,a30,2f20.10)') i, filter(i)% filename, ZP(i), -2.5d0*log10(ZP(i))
+      enddo
+
+      open(99,file=trim(spectra_list))
+      do while(.true.)
+
+         select case(choice)
+         case(PHOENIX)
+            read(99,'(a)',iostat=ierr) filename
+            if(ierr/=0) exit
+            if(filename(1:1)=='#') cycle
+            prefix=filename(:index(filename,'.')-1)
+            filename=trim(data_dir)//'/PHOENIX/lists/'//trim(filename)
+            write(*,*) trim(filename), ' ', trim(prefix)
+            call read_phoenix(filename,spectra,num_spectra,ierr)
+         case(CK2003)
+            line=''
+            read(99,'(a)',iostat=ierr) line
+            if(ierr/=0) exit
+            i0=index(line,'  Z')
+            filename=trim(adjustl(line(:i0)))
+            prefix=trim(adjustl(line(i0:)))
+            write(*,*) trim(filename), ' ', trim(prefix)
+            call read_ck2003(filename,spectra,num_spectra,ierr)
+         case(ATLAS)
+            read(99,'(a)',iostat=ierr) filename
+            if(ierr/=0) exit
+            if(filename(1:1)=='#') cycle
+            prefix=filename(:index(filename,'.')-1)
+            filename=trim(data_dir)//'/ATLAS/lists/'//trim(filename)
+            write(*,*) trim(filename), ' ', trim(prefix)
+            call read_ATLAS(filename,spectra,num_spectra,ierr)
+         end select
+
+         nullify(mag)
+         allocate(mag(num_filters,num_Av,num_Rv,num_spectra))
+
+         !main loop where BCs are calculated
+         AOK = .true.
+!$omp parallel do private(i,j,k,l)
+         do i=1,num_spectra
+!$omp critical
+            if(read_on_the_fly) call load_spec(choice,spectra(i),ierr)
+!$omp end critical
+            if(ierr/=0) cycle
+            do j=1,num_Rv
+               do k=1,num_Av
+                  call extinction_for_spectrum(spectra(i),Av(k),Rv(j))
+                  do l=1,num_filters
+                     tmp_mag = const*integrate_bandpass(spectra(i),filter(l),ierr)/(spectra(i)% Fbol * ZP(l))
+                     mag(l,k,j,i) = Msolbol + 2.5d0*log10(tmp_mag)
+                     if(ierr/=0) then
+                        write(0,*) 'failed in integrate_bandpass'
+                        write(0,*) ' file = ', trim(spectra(i)% filename)
+                        AOK = .false.
+                        exit
+                     endif
+                  enddo !l-loop
+               enddo !k-loop
+            enddo !j-loop
+!$omp critical
+            if(choice/=CK2003.and.read_on_the_fly) call unload_spec(spectra(i),ierr)
+!$omp end critical
+         enddo
+!$omp end parallel do
+
+         if(.not.AOK) then
+            write(0,*) "A complete flop and the worst thing I've ever done."
+            return
+         endif
+
+         do j=1,num_Rv
+            write(cRv,'(i2)') int(10*Rv(j))
+            select case(choice)
+            case(PHOENIX)
+               outfile=trim(data_dir)//'/phx/'//trim(prefix)//trim(suffix)//'.Rv' // cRv
+            case(CK2003)
+               outfile=trim(data_dir)//'/kur/'//trim(prefix)//trim(suffix)//'.Rv' // cRv
+            case(ATLAS)
+               outfile=trim(data_dir)//'/kur/'//trim(prefix)//trim(suffix)//'.Rv' // cRv
+            end select
+            write(0,*) '   output to ', trim(outfile)
+            open(1,file=trim(outfile))
+            write(1,1) '#', num_spectra, num_Av
+            do k=1,num_Av
+               write(1,2) '#', (i,i=1,num_filters+5)
+               write(1,3) '#', 'Teff ', 'logg ', '[Fe/H]', '  Av ', '  Rv ', filter_name(1:num_filters)
+               do i=1,num_spectra
+                  write(1,4) spectra(i)% Teff, spectra(i)% logg, spectra(i)% FeH, Av(k), Rv(j), mag(:,k,j,i)
+               enddo
+               write(1,*)
+               write(1,*)
+            enddo
+            close(1)
+         enddo
+         deallocate(mag,spectra)
+      enddo
+      close(99)
+      
+      deallocate(filter,ZP,filter_name)
+
+ 1    format(a1,1x,2i4)
+ 2    format(a1,i5, i5, 3i6, 99(5x,i2,5x))
+ 3    format(a1,a5, a5, 3a6 ,99a12)
+ 4    format(f6.0,f5.1,3f6.2,99f12.6)
+
+      end subroutine do_one_set
+
+      subroutine setup(phot_system)
+      integer, intent(in) :: phot_system
+
+      select case(phot_system)
+      case(HST_WFC3)
+         write(*,*) ' doing HST/WFC3'
+         filter_list = 'lists/wfc3_filter.list'
+         suffix = '.HST_WFC3'
+         zero_point_type = zero_point_Vega
+      case(HST_ACS_WFC)
+         write(*,*) ' doing HST/ACS-WFC'
+         filter_list = 'lists/acs_wfc_filter.list'
+         suffix = '.HST_ACSWF'
+         zero_point_type = zero_point_Vega
+      case(HST_ACS_HRC)
+         write(*,*) ' doing HST/ACS-HRC'
+         filter_list = 'lists/acs_hrc_filter.list'
+         suffix = '.HST_ACSHR'
+         zero_point_type = zero_point_Vega
+      case(HST_WFPC2)
+         write(*,*) ' doing HST/WFPC2'
+         filter_list = 'lists/wfpc2_filter.list'
+         suffix = '.HST_WFPC2'
+         zero_point_type = zero_point_Vega
+      case(SDSS)
+         write(*,*) ' doing SDSS'
+         filter_list = 'lists/SDSS_filter.list'
+         suffix = '.SDSSugriz'
+         zero_point_type = zero_point_AB
+      case(CFHT)
+         write(*,*) ' doing CFHT'
+         filter_list = 'lists/CFHT_filter.list'
+         suffix = '.CFHTugriz'
+         zero_point_type = zero_point_AB
+      case(UBVRIJHKsKp)
+         write(*,*) ' doing UBVRI+2MASS+Kepler'
+         filter_list = 'lists/UBVRIJHKsKpD51_filter.list'
+         suffix = '.UBVRIJHKsKp'
+         zero_point_type = zero_point_Vega
+      case(WashDDOuvby)
+         write(*,*) ' doing Washington+DDO51+Stroemgren'
+         filter_list = 'lists/WashDDOuvby_filter.list'
+         suffix = '.WashDDOuvby'
+         zero_point_type = zero_point_Vega
+      case(UKIDSS)
+         write(*,*) ' doing UKIDSS'
+         filter_list = 'lists/UKIDSS_filter.list'
+         suffix = '.UKIDSS'
+         zero_point_type = zero_point_Vega
+      case(WISE)
+         write(*,*) ' doing WISE'
+         filter_list = 'lists/WISE_filter.list'
+         suffix = '.WISE'
+         zero_point_type = zero_point_Vega
+      case(PanSTARRS)
+         write(*,*) ' doing PanSTARRS'
+         filter_list = 'lists/PanSTARRS1_filter.list'
+         suffix = '.PanSTARRS'
+         zero_point_type = zero_point_AB
+      case(SPITZER)
+         write(*,*) ' doing SPITZER'
+         filter_list = 'lists/SPITZER_filter.list'
+         suffix = '.SPITZER'
+         zero_point_type = zero_point_Vega
+      case(SkyMapper)
+         write(*,*) ' doing SkyMapper'
+         filter_list = 'lists/SkyMapper_filter.list'
+         suffix='.SkyMapper'
+         zero_point_type = zero_point_AB
+      case(LSST)
+         write(*,*) ' doing LSST'
+         filter_list = 'lists/LSST_filter.list'
+         suffix='.LSST'
+         zero_point_type = zero_point_AB
+      case(Swift)
+         write(*,*) ' doing Swift UVOT'
+         filter_list = 'lists/swift.list'
+         suffix='.Swift'
+         zero_point_type = zero_point_AB
+      case default
+         filter_list = ''
+         suffix = ''
+         zero_point_type = 0
+      end select
+
+      end subroutine setup
+
+      subroutine write_usage_details
+         write(*,*) ' usage: ./bandpass [M] [N]'
+         write(*,*) '                      '
+         write(*,*) '        M = 0 - 3     '
+         write(*,*) ' Vega/AB/ST ZPs =  0  '
+         write(*,*) ' PHOENIX        =  1  '
+         write(*,*) ' Castelli&Kurucz=  2  '
+         write(*,*) ' SYNTHE high res=  3  '
+         write(*,*) '                      '
+         write(*,*) '        N = 1 - 14    '
+         write(*,*) ' HST_WFC3       =  1  '
+         write(*,*) ' HST_ACS_WFC    =  2  '
+         write(*,*) ' HST_ACS_HRC    =  3  '
+         write(*,*) ' HST_WFPC2      =  4  '
+         write(*,*) ' SDSS           =  5  '
+         write(*,*) ' CFHT/MegaCam   =  6  '
+         write(*,*) ' UBVRIJHKsKp    =  7  '
+         write(*,*) ' WashDDOuvby    =  8  '
+         write(*,*) ' UKIDSS         =  9  '
+         write(*,*) ' WISE           = 10  '
+         write(*,*) ' PanSTARRS      = 11  '
+         write(*,*) ' SPITZER        = 12  '
+         write(*,*) ' SkyMapper      = 13  '
+         write(*,*) ' LSST           = 14  '
+         write(*,*) ' Swift          = 15  '
+         write(*,*)
+      end subroutine write_usage_details
+
+      end program test_bandpass
